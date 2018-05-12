b6bc947153080f7568acee8e61e37fcdc89ca86d
Martin Bayly <Martin.Bayly@D2L.com>
2018-05-11 19:28:54 -0700

    adding tests for editing criterion name

--- test/components/d2l-rubric-criterion-editor.js
+++ test/components/d2l-rubric-criterion-editor.js
@@ -1,4 +1,4 @@
-/* global suite, test, fixture, expect, suiteSetup, suiteTeardown, sinon */
+/* global suite, test, fixture, expect, teardown, suiteSetup, suiteTeardown, sinon */
 
 'use strict';
 
@@ -21,5 +21,47 @@ suite('<d2l-rubric-criterion-editor>', function() {
 			expect(element.is).to.equal('d2l-rubric-criterion-editor');
 		});
 
+		suite('edit name', function() {
+
+			var fetch;
+			var raf = window.requestAnimationFrame;
+
+			teardown(function() {
+				fetch && fetch.restore();
+			});
+
+			// setup(function() {
+			// 	fetch = sinon.stub(window.d2lfetch, 'fetch');
+			// 	var promise = Promise.resolve({
+			// 		ok: true,
+			// 		json: function() {
+			// 			return Promise.resolve(JSON.stringify(window.testFixtures.assignmentHomeCanCreate));
+			// 		}
+			// 	});
+			// 	window.d2lfetch.fetch.returns(promise);
+			// });
+
+			test.only('saves name', function(done) {
+				fetch = sinon.stub(window.d2lfetch, 'fetch');
+				var promise = Promise.resolve({
+					ok: true,
+					json: function() {
+						return Promise.resolve(JSON.stringify(element.entity));
+					}
+				});
+				fetch.returns(promise);
+
+				raf(function() {
+					var nameTextArea = element.$$('d2l-textarea');
+					nameTextArea.value = 'Batman and Robin';
+					nameTextArea.textarea.dispatchEvent(new Event('change', { bubbles: true, cancelable: false, composed: false }));
+					element.addEventListener('d2l-rubric-criterion-saved', function() {
+						var body = fetch.args[0][1].body;
+						expect(body.get('name')).to.equal('Batman and Robin');
+						done();
+					});
+				});
+			});
+		});
 	});
 });
