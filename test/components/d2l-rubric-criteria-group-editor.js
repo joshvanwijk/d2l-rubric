22842516cc3bd8eff2320cede2f99b6bb54d90e8
Emil Furniss <Emil.Furniss@D2L.com>
2018-07-24 10:41:03 -0700

    readd basic unit test setup

--- test/components/d2l-rubric-criteria-group-editor.js
+++ test/components/d2l-rubric-criteria-group-editor.js
@@ -21,5 +21,27 @@ suite('<d2l-rubric-criteria-group-editor>', function() {
 		test('can be instantiated', function() {
 			expect(element.is).to.equal('d2l-rubric-criteria-group-editor');
 		});
+
+		suite('edit group name', function() {
+
+			var element;
+
+			setup(function(done) {
+				element = fixture('basic-criteria');
+				function waitForLoad(e) {
+					if (e.detail.entity.getLinkByRel('self').href === 'static-data/rubrics/organizations/text-only/199/groups/176.json') {
+						element.removeEventListener('d2l-rubric-entity-changed', waitForLoad);
+						done();
+					}
+				}
+				element.addEventListener('d2l-rubric-entity-changed', waitForLoad);
+				element.token = 'foozleberries';
+			});
+
+			test('group name editor enabled with action', function(done) {
+				expect(element.$$('#group-name').disabled).to.be.false;
+				done();
+			});
+		});
 	});
 });
