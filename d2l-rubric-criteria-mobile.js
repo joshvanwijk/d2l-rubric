14f2a8f0f9040022f2379e281ce1f1428736aa03
Xiangqi Yan <Xiangqi.Yan@D2L.com>
2019-10-29 15:13:20 -0400

    fix invalid feedback disappearing if add feedback button is clicked

--- d2l-rubric-criteria-mobile.js
+++ d2l-rubric-criteria-mobile.js
@@ -42,8 +42,8 @@ $_documentContainer.innerHTML = `<dom-module id="d2l-rubric-criteria-mobile">
 		<template is="dom-repeat" items="[[_criteria]]" as="criterion" index-as="criterionNum">
 			<d2l-rubric-criterion-mobile href="[[_getSelfLink(criterion)]]" levels-href="[[levelsHref]]" assessment-href="[[assessmentHref]]" token="[[token]]" is-holistic="[[isHolistic]]" is-numeric="[[isNumeric]]" read-only="[[readOnly]]">
 			</d2l-rubric-criterion-mobile>
-			<d2l-button-subtle class="add-feedback-button" hidden="[[!_addFeedback(criterion, assessmentResult, criterionNum, _addingFeedback, _savingFeedback)]]" text="[[localize('addFeedback')]]" on-click="_handleAddFeedback" data-criterion$="[[criterionNum]]"></d2l-button-subtle>
-			<template is="dom-if" if="[[_displayFeedback(_feedbackDisplay, criterionNum, _addingFeedback, _savingFeedback)]]">
+			<d2l-button-subtle class="add-feedback-button" hidden="[[!_addFeedback(criterion, assessmentResult, criterionNum, _addingFeedback, _savingFeedback, _feedbackInvalid)]]" text="[[localize('addFeedback')]]" on-click="_handleAddFeedback" data-criterion$="[[criterionNum]]"></d2l-button-subtle>
+			<template is="dom-if" if="[[_displayFeedback(_feedbackDisplay, criterionNum, _addingFeedback, _savingFeedback, _feedbackInvalid)]]">
 				<d2l-rubric-feedback id="feedback[[criterionNum]]" criterion-href="[[_getSelfLink(criterion)]]" assessment-href="[[assessmentHref]]" token="[[token]]" adding-feedback="[[_cellAddingFeedback(criterionNum, _addingFeedback)]]" on-save-feedback="_handleSaveFeedback" on-save-feedback-finished="_handleSaveFinished" on-close-feedback="_closeFeedback">
 				</d2l-rubric-feedback>
 			</template>
@@ -84,6 +84,12 @@ Polymer({
 				return [];
 			}
 		},
+		_feedbackInvalid: {
+			type: Array,
+			value: function() {
+				return [];
+			}
+		}
 	},
 
 	behaviors: [
@@ -131,7 +137,7 @@ Polymer({
 		}
 	},
 
-	_addFeedback: function(entity, assessmentResult, criterionNum, addingFeedback, savingFeedback) {
+	_addFeedback: function(entity, assessmentResult, criterionNum, addingFeedback, savingFeedback, feedbackInvalid) {
 		if (!entity || !assessmentResult) {
 			return false;
 		}
@@ -141,17 +147,17 @@ Polymer({
 		if (!this.canAddFeedback(entity)) {
 			return false;
 		}
-		if (criterionNum === addingFeedback || savingFeedback.includes(criterionNum)) {
+		if (criterionNum === addingFeedback || savingFeedback.includes(criterionNum) || feedbackInvalid[criterionNum]) {
 			return false;
 		}
 		return !this._hasFeedback(entity, assessmentResult);
 	},
 
-	_displayFeedback: function(feedbackDisplay, criterionNum, addingFeedback, savingFeedback) {
+	_displayFeedback: function(feedbackDisplay, criterionNum, addingFeedback, savingFeedback, feedbackInvalid) {
 		if (!feedbackDisplay) {
 			return;
 		}
-		return feedbackDisplay[criterionNum] || criterionNum === addingFeedback || savingFeedback.includes(criterionNum);
+		return feedbackDisplay[criterionNum] || criterionNum === addingFeedback || savingFeedback.includes(criterionNum) || feedbackInvalid[criterionNum];
 	},
 
 	_cellAddingFeedback: function(addingFeedback, criterionNum) {
@@ -175,6 +181,8 @@ Polymer({
 		var criterionNum = event.model.get('criterionNum');
 		var index = this._savingFeedback.indexOf(criterionNum);
 		this.splice('_savingFeedback', index, 1);
+
+		this.set(['_feedbackInvalid', criterionNum], !event.detail.success);
 	},
 
 	_closeFeedback: function() {
