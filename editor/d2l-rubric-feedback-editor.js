cc567106dd86f9284b1edb482f9957a510653991
Xiangqi Yan <Xiangqi.Yan@D2L.com>
2019-12-06 16:25:20 -0500

    fix table border disappearing

--- editor/d2l-rubric-feedback-editor.js
+++ editor/d2l-rubric-feedback-editor.js
@@ -24,6 +24,7 @@ $_documentContainer.innerHTML = /*html*/`<dom-module id="d2l-rubric-feedback-edi
 					box-shadow: none;
 					border-radius: 0;
 					transition-property: none;
+					background-color: transparent;
 				};
 			}
 
